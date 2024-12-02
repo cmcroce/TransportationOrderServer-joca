e881db087bd76abf09cb0b758850084bad4a44b2
cmcroce <c.corce@alumnos.upm.es>
2024-12-02 17:55:11 +0100

    First set of tests, adding ci yml file

--- src/test/java/es/upm/dit/apsv/transportationorderserver/TransportationOrderServerApplicationTests.java
+++ src/test/java/es/upm/dit/apsv/transportationorderserver/TransportationOrderServerApplicationTests.java
@@ -1,15 +1,151 @@
 package es.upm.dit.apsv.transportationorderserver;
 
+import static org.mockito.Mockito.when;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.hamcrest.Matchers.*;
+
+import java.io.BufferedReader;
+
+import java.io.FileReader;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+import java.util.Arrays;
+
+import java.util.List;
+
+import java.util.Optional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootTest
-@SpringBootApplication
-class TransportationOrderServerApplicationTests {
+import org.mockito.InjectMocks;
 
-	@Test
-	void contextLoads() {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.core.io.ClassPathResource;
+
+import org.springframework.http.MediaType;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.test.web.servlet.MvcResult;
+
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import es.upm.dit.apsv.transportationorderserver.repository.TransportationOrderRepository;
+
+import es.upm.dit.apsv.transportationorderserver.controller.TransportationOrderController;
+
+import es.upm.dit.apsv.transportationorderserver.model.TransportationOrder;
+
+@WebMvcTest(TransportationOrderController.class)
+
+public class TransportationOrderServerApplicationTests {
+
+    @InjectMocks
+
+    private TransportationOrderController business;
+
+    @MockBean
+
+    private TransportationOrderRepository repository;
+
+    @Autowired
+
+    private MockMvc mockMvc;
+
+    @Test
+
+    public void testGetOrders() throws Exception {
+
+        when(repository.findAll()).thenReturn(getAllTestOrders()); //configure the Mock repository, so that it returns a list of 20 orders whenever its findAll() method is invoked, 
+
+        RequestBuilder request = MockMvcRequestBuilders
+
+                .get("/transportationorders") //call GET "/transportationorders"  application/json
+
+                .accept(MediaType.APPLICATION_JSON);
+
+       
+
+        MvcResult result = mockMvc.perform(request)
+
+                .andExpect(status().isOk())
+
+                .andExpect(jsonPath("$", hasSize(20))) //verify that the HTTP response from the controller is both 200, and has 20 elements as a JSON
+
+                .andReturn();
+
+    }
+
+    private List<TransportationOrder> getAllTestOrders(){
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ArrayList<TransportationOrder> orders =
+
+               new ArrayList<TransportationOrder>();
+
+        TransportationOrder order = null;
+
+       
+
+        try(BufferedReader br = new BufferedReader(new FileReader(
+
+                        new ClassPathResource("orders.json").getFile()))) {
+
+            for(String line; (line = br.readLine()) != null; ) {
+
+              order = objectMapper.readValue(line, TransportationOrder.class);
+
+              orders.add(order);
+
+            }
+
+          } catch (IOException e) {
+
+                e.printStackTrace();
+
+        }
+
+         return orders;
+
+       }
+
+	   @Test
+
+	public void testGetOrder() throws Exception {
+
+     //call GET "/transportationorders/{truck}"  application/json
+       
+     	when(repository.findById("8962ZKR")).thenReturn(Optional.of(
+              	new TransportationOrder("28","8962ZKR",1591682400000L,
+            	  40.4562191,-3.8707211,1591692196000L,42.0206372,-4.5330132,
+            	  0,0.0,0.0,0)));
+     	// now write the rest of the test case...
 	}
+
+	public void testGetOrder2() throws Exception {
+
+		//call GET "/transportationorders/{truck}"  application/json
+		  
+			when(repository.findById("8888AAA")).thenReturn(Optional.of(
+					 new TransportationOrder("28","8962ZKR",1591682400000L,
+					 40.4562191,-3.8707211,1591692196000L,42.0206372,-4.5330132,
+					 0,0.0,0.0,0)));
+			// now write the rest of the test case...
+	   }
 
 }
